2d12c0d7f4f595ce141910e1dde10787c987ec8e
MistyIslam5 <mdmisty5@gmail.com>
2024-10-22 11:06:29 +0600

    test repo

---
+++ src/components/Bolgs/Blogs.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Blogs = () => {
+
+    const [bolgs, setBogs] = useState([])
+
+    useEffect
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
